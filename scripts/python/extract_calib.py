b6a89a9a99b2aa67a3ba8461713a589b9f63e4ae
Nick Le Large <nick.lelarge@kit.edu>
2021-04-23 09:21:54 +0200

    Added scripts to extract and plot camera transformations. WIP.

---
+++ scripts/python/extract_calib.py
@@ -0,0 +1,234 @@
+# This script is based on read_write_model.py and is used to extract the
+# estimated intrinsic and extrinsic camera parameters of a sparse model.
+# Author: Nick Le Large
+
+import os
+import sys
+import collections
+import numpy as np
+import struct
+import argparse
+
+
+CameraModel = collections.namedtuple(
+    "CameraModel", ["model_id", "model_name", "num_params"])
+Camera = collections.namedtuple(
+    "Camera", ["id", "model", "width", "height", "params"])
+BaseImage = collections.namedtuple(
+    "Image", ["id", "qvec", "tvec", "camera_id", "name", "xys", "point3D_ids"])
+
+
+class Image(BaseImage):
+    def qvec2rotmat(self):
+        return qvec2rotmat(self.qvec)
+
+
+CAMERA_MODELS = {
+    CameraModel(model_id=0, model_name="SIMPLE_PINHOLE", num_params=3),
+    CameraModel(model_id=1, model_name="PINHOLE", num_params=4),
+    CameraModel(model_id=2, model_name="SIMPLE_RADIAL", num_params=4),
+    CameraModel(model_id=3, model_name="RADIAL", num_params=5),
+    CameraModel(model_id=4, model_name="OPENCV", num_params=8),
+    CameraModel(model_id=5, model_name="OPENCV_FISHEYE", num_params=8),
+    CameraModel(model_id=6, model_name="FULL_OPENCV", num_params=12),
+    CameraModel(model_id=7, model_name="FOV", num_params=5),
+    CameraModel(model_id=8, model_name="SIMPLE_RADIAL_FISHEYE", num_params=4),
+    CameraModel(model_id=9, model_name="RADIAL_FISHEYE", num_params=5),
+    CameraModel(model_id=10, model_name="THIN_PRISM_FISHEYE", num_params=12)
+}
+CAMERA_MODEL_IDS = dict([(camera_model.model_id, camera_model)
+                         for camera_model in CAMERA_MODELS])
+CAMERA_MODEL_NAMES = dict([(camera_model.model_name, camera_model)
+                           for camera_model in CAMERA_MODELS])
+
+
+def read_next_bytes(fid, num_bytes, format_char_sequence, endian_character="<"):
+    """Read and unpack the next bytes from a binary file.
+    :param fid:
+    :param num_bytes: Sum of combination of {2, 4, 8}, e.g. 2, 6, 16, 30, etc.
+    :param format_char_sequence: List of {c, e, f, d, h, H, i, I, l, L, q, Q}.
+    :param endian_character: Any of {@, =, <, >, !}
+    :return: Tuple of read and unpacked values.
+    """
+    data = fid.read(num_bytes)
+    return struct.unpack(endian_character + format_char_sequence, data)
+
+
+def write_next_bytes(fid, data, format_char_sequence, endian_character="<"):
+    """pack and write to a binary file.
+    :param fid:
+    :param data: data to send, if multiple elements are sent at the same time,
+    they should be encapsuled either in a list or a tuple
+    :param format_char_sequence: List of {c, e, f, d, h, H, i, I, l, L, q, Q}.
+    should be the same length as the data list or tuple
+    :param endian_character: Any of {@, =, <, >, !}
+    """
+    if isinstance(data, (list, tuple)):
+        bytes = struct.pack(endian_character + format_char_sequence, *data)
+    else:
+        bytes = struct.pack(endian_character + format_char_sequence, data)
+    fid.write(bytes)
+
+
+def read_cameras_binary(path_to_model_file):
+    """
+    see: src/base/reconstruction.cc
+        void Reconstruction::WriteCamerasBinary(const std::string& path)
+        void Reconstruction::ReadCamerasBinary(const std::string& path)
+    """
+    cameras = {}
+    with open(path_to_model_file, "rb") as fid:
+        num_cameras = read_next_bytes(fid, 8, "Q")[0]
+        for _ in range(num_cameras):
+            camera_properties = read_next_bytes(
+                fid, num_bytes=24, format_char_sequence="iiQQ")
+            camera_id = camera_properties[0]
+            model_id = camera_properties[1]
+            model_name = CAMERA_MODEL_IDS[camera_properties[1]].model_name
+            width = camera_properties[2]
+            height = camera_properties[3]
+            num_params = CAMERA_MODEL_IDS[model_id].num_params
+            params = read_next_bytes(fid, num_bytes=8*num_params,
+                                     format_char_sequence="d"*num_params)
+            cameras[camera_id] = Camera(id=camera_id,
+                                        model=model_name,
+                                        width=width,
+                                        height=height,
+                                        params=np.array(params))
+        assert len(cameras) == num_cameras
+    return cameras
+
+
+def write_intrinsic(cameras, path):
+    """
+    see: src/base/reconstruction.cc
+        void Reconstruction::WriteCamerasText(const std::string& path)
+        void Reconstruction::ReadCamerasText(const std::string& path)
+    """
+    HEADER = "# Camera list with one line of data per camera:\n" \
+    "#   CAMERA_ID, MODEL, WIDTH, HEIGHT, PARAMS[]\n" \
+    "# Number of cameras: {}\n".format(len(cameras))
+    with open(path, "w") as fid:
+        fid.write(HEADER)
+        for _, cam in cameras.items():
+            to_write = [cam.id, cam.model, cam.width, cam.height, *cam.params]
+            line = " ".join([str(elem) for elem in to_write])
+            fid.write(line + "\n")
+
+
+def read_images_binary(path_to_model_file):
+    """
+    see: src/base/reconstruction.cc
+        void Reconstruction::ReadImagesBinary(const std::string& path)
+        void Reconstruction::WriteImagesBinary(const std::string& path)
+    """
+    images = {}
+    with open(path_to_model_file, "rb") as fid:
+        num_reg_images = read_next_bytes(fid, 8, "Q")[0]
+        for _ in range(num_reg_images):
+            binary_image_properties = read_next_bytes(
+                fid, num_bytes=64, format_char_sequence="idddddddi")
+            image_id = binary_image_properties[0]
+            qvec = np.array(binary_image_properties[1:5])
+            tvec = np.array(binary_image_properties[5:8])
+            camera_id = binary_image_properties[8]
+            image_name = ""
+            current_char = read_next_bytes(fid, 1, "c")[0]
+            while current_char != b"\x00":   # look for the ASCII 0 entry
+                image_name += current_char.decode("utf-8")
+                current_char = read_next_bytes(fid, 1, "c")[0]
+            num_points2D = read_next_bytes(fid, num_bytes=8,
+                                           format_char_sequence="Q")[0]
+            x_y_id_s = read_next_bytes(fid, num_bytes=24*num_points2D,
+                                       format_char_sequence="ddq"*num_points2D)
+            xys = np.column_stack([tuple(map(float, x_y_id_s[0::3])),
+                                   tuple(map(float, x_y_id_s[1::3]))])
+            point3D_ids = np.array(tuple(map(int, x_y_id_s[2::3])))
+            images[image_id] = Image(
+                id=image_id, qvec=qvec, tvec=tvec,
+                camera_id=camera_id, name=image_name,
+                xys=xys, point3D_ids=point3D_ids)
+    return images
+
+
+def write_extrinsic(images, path):
+    """
+    see: src/base/reconstruction.cc
+        void Reconstruction::ReadImagesText(const std::string& path)
+        void Reconstruction::WriteImagesText(const std::string& path)
+    """
+    HEADER = "# Image list with two lines of data per image:\n" \
+    "#   IMAGE_ID, QW, QX, QY, QZ, TX, TY, TZ, CAMERA_ID, NAME\n" \
+    "# Number of images: {}\n".format(len(images))
+
+    with open(path, "w") as fid:
+        fid.write(HEADER)
+        for _, img in images.items():
+            image_header = [img.id, *img.qvec, *img.tvec, img.camera_id, img.name]
+            line = " ".join(map(str, image_header))
+            fid.write(line + "\n")
+
+
+def read_model(path):
+    if not os.path.isfile(os.path.join(path, "cameras.bin")) or \
+        not os.path.isfile(os.path.join(path, "images.bin")):
+            raise RuntimeError('Failed to open mode. Folder must contain cameras.bin and images.bin')
+
+    cameras = read_cameras_binary(os.path.join(path, "cameras.bin"))
+    images = read_images_binary(os.path.join(path, "images.bin"))
+
+    return cameras, images
+
+
+def write_model(cameras, images, path):
+
+    write_intrinsic(cameras, os.path.join(path, "intrinsic.txt"))
+    write_extrinsic(images, os.path.join(path, "extrinsic.txt"))
+
+    return cameras, images
+
+
+def qvec2rotmat(qvec):
+    return np.array([
+        [1 - 2 * qvec[2]**2 - 2 * qvec[3]**2,
+         2 * qvec[1] * qvec[2] - 2 * qvec[0] * qvec[3],
+         2 * qvec[3] * qvec[1] + 2 * qvec[0] * qvec[2]],
+        [2 * qvec[1] * qvec[2] + 2 * qvec[0] * qvec[3],
+         1 - 2 * qvec[1]**2 - 2 * qvec[3]**2,
+         2 * qvec[2] * qvec[3] - 2 * qvec[0] * qvec[1]],
+        [2 * qvec[3] * qvec[1] - 2 * qvec[0] * qvec[2],
+         2 * qvec[2] * qvec[3] + 2 * qvec[0] * qvec[1],
+         1 - 2 * qvec[1]**2 - 2 * qvec[2]**2]])
+
+
+def rotmat2qvec(R):
+    Rxx, Ryx, Rzx, Rxy, Ryy, Rzy, Rxz, Ryz, Rzz = R.flat
+    K = np.array([
+        [Rxx - Ryy - Rzz, 0, 0, 0],
+        [Ryx + Rxy, Ryy - Rxx - Rzz, 0, 0],
+        [Rzx + Rxz, Rzy + Ryz, Rzz - Rxx - Ryy, 0],
+        [Ryz - Rzy, Rzx - Rxz, Rxy - Ryx, Rxx + Ryy + Rzz]]) / 3.0
+    eigvals, eigvecs = np.linalg.eigh(K)
+    qvec = eigvecs[[3, 0, 1, 2], np.argmax(eigvals)]
+    if qvec[0] < 0:
+        qvec *= -1
+    return qvec
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Extract estimated intrinsic and extrinsic calibration from binary colmap model.")
+    parser.add_argument("--input_model", help="path to input model folder")
+    parser.add_argument("--output_model", help="path to output model folder")
+    args = parser.parse_args()
+
+    cameras, images = read_model(path=args.input_model)
+
+    print("Number of estimated intrinsics:", len(cameras))
+    print("Number of estimated extrinsics:", len(images))
+
+    if args.output_model is not None:
+        write_model(cameras, images, path=args.output_model)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/python/plot_extrinsic.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python2
+# Quick script to visually validate extrinsics extracted using extract_calib.py
+# Author: Nick Le Large
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
+import argparse
+import sys
+from transforms3d import affines, quaternions
+
+Transformation = collections.namedtuple(
+    "Transformation", ["id", "qvec", "tvec", "camera_id", "name"])
+
+def read_extrinsic(path):
+    transformations = {}
+    with open(path, "r") as fid:
+        while True:
+            line = fid.readline()
+            if not line:
+                break
+            line = line.strip()
+            if len(line) > 0 and line[0] != "#":
+                elems = line.split()
+                image_id = int(elems[0])
+                qvec = np.array(tuple(map(float, elems[1:5])))
+                tvec = np.array(tuple(map(float, elems[5:8])))
+                camera_id = int(elems[8])
+                image_name = elems[9]
+                transformations[image_id] = Transformation(
+                    id=image_id, qvec=qvec, tvec=tvec,
+                    camera_id=camera_id, name=image_name)
+    return transformations
+
+def draw_cos(ax, tf, axislength = 0.2, text = ''):
+    # Definitions in local coordinate frame
+    origin_loc = [0, 0, 0, 1]
+    x_loc = [axislength, 0, 0, 1]
+    y_loc = [0, axislength, 0, 1]
+    z_loc = [0, 0, axislength, 1]
+    text_pos_loc = [0, 0, -0.1, 1]
+
+    # Transform using given tf
+    origin_glob = np.matmul(tf, origin_loc)
+    x_glob = np.matmul(tf, x_loc)
+    y_glob = np.matmul(tf, y_loc)
+    z_glob = np.matmul(tf, z_loc)
+    text_pos_glob = np.matmul(tf, text_pos_loc)
+    
+    # Plot axes
+    ax.plot([origin_glob[0], x_glob[0]], [origin_glob[1],x_glob[1]], [origin_glob[2],x_glob[2]], color='r')
+    ax.plot([origin_glob[0], y_glob[0]], [origin_glob[1],y_glob[1]], [origin_glob[2],y_glob[2]], color='g')
+    ax.plot([origin_glob[0], z_glob[0]], [origin_glob[1],z_glob[1]], [origin_glob[2],z_glob[2]], color='b')
+
+    # Add text
+    ax.text(text_pos_glob[0], text_pos_glob[1], text_pos_glob[2], text)
+
+def fix_axes(ax, coord_range):
+    # Hacky way of fixing axes ranges by plotting invisible cube with maximum range
+    Xmin = coord_range[0,0]
+    Xmax = coord_range[0,1]
+    Ymin = coord_range[1,0]
+    Ymax = coord_range[1,1]
+    Zmin = coord_range[2,0]
+    Zmax = coord_range[2,1]
+
+    max_range = np.array([Xmax-Xmin, Ymax-Ymin, Zmax-Zmin]).max()
+    Xb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][0].flatten() + 0.5*(Xmax+Xmin)
+    Yb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][1].flatten() + 0.5*(Ymax+Ymin)
+    Zb = 0.5*max_range*np.mgrid[-1:2:2,-1:2:2,-1:2:2][2].flatten() + 0.5*(Zmax+Zmin)
+    for xb, yb, zb in zip(Xb, Yb, Zb):
+        ax.plot([xb], [yb], [zb], 'w')
+
+def plot_extrinsic(extrinsic):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    coord_range = np.block([[np.ones((3,1)) * sys.float_info.max, np.ones((3,1)) * sys.float_info.min]])
+    for _, e in extrinsic.items():
+        t = e.tvec        
+        R = quaternions.quat2mat(e.qvec)
+        tf = affines.compose(t, R, np.ones(3))
+        draw_cos(ax, tf, axislength = 0.2, text = '')
+
+        for idx, coord in enumerate(t):
+            if coord < coord_range[idx, 0]:
+                coord_range[idx, 0] = coord
+            if coord > coord_range[idx, 1]:
+                coord_range[idx, 1] = coord
+
+    fix_axes(ax, coord_range)
+
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Read and write COLMAP binary and text models")
+    parser.add_argument("extrinsic_calib", help="path to textfile containing extrinsic calibration.")
+    args = parser.parse_args()
+
+    extrinsic = read_extrinsic(path=args.extrinsic_calib)
+    plot_extrinsic(extrinsic)
